9f92658769f8a49d2209755e1180a0d04fd70b52
azeeee05 <azeemazm31@gmail.com>
2025-11-27 20:15:10 +0530

    fixed ui

--- src/lib/apiClient.ts
+++ src/lib/apiClient.ts
@@ -1,9 +1,12 @@
 import axios from "axios";
 import { getCookie, deleteCookie } from "./cookies";
+import { API_BASE_URL } from "../constant";
+ 
 
-const API_BASE_URL =
-  import.meta.env.VITE_API_BASE_URL || "http://localhost:4000/api/v1";
-
+const apiClient = axios.create({
+  baseURL: API_BASE_URL,
+  timeout: 15000,
+});
 const apiClient = axios.create({
   baseURL: API_BASE_URL,
   timeout: 15000,
